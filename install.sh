ea1186e66679ec5e4533aae2e761c6f31c707986
Artyom Prikhodko <artyom.prikhodko@jetbrains.com>
2023-03-17 21:27:25 +0100

    add brew installation

--- install.sh
+++ install.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
+if test ! $(which brew); then
+    echo "Installing homebrew"
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
+echo "Installing homebrew packages..."
+
+# development tools
+brew install git
+brew install iterm2 --cask
+
+brew install tmux
+brew install zsh
+brew install markdown
+brew install zsh-syntax-highlighting
+brew install zsh-autosuggestions
+
+brew install nvm
+
+echo "Installing neovim..."
+brew install neovim
 
