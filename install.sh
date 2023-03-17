8344ed1f377e507e8612eee25ec50bbb920bbf33
Artyom Prikhodko <artyom.prikhodko@jetbrains.com>
2023-03-17 22:01:01 +0100

    add symlinks

--- install.sh
+++ install.sh
@@ -22,3 +22,21 @@ brew install nvm
 echo "Installing neovim..."
 brew install neovim
 
+echo -e "\n\ninstalling to ~/.config"
+echo "=============================="
+if [ ! -d $HOME/.config ]; then
+    echo "Creating ~/.config"
+    mkdir -p $HOME/.config
+fi
+
+for config in ./config/*; do
+    echo $config
+    #target=$HOME/.config/$( basename $config )
+    #if [ -e $target ]; then
+    #    echo "~${target#$HOME} already exists... Skipping."
+    #else
+    #    echo "Creating symlink for $config"
+    #    ln -s $config $target
+    #fi
+done
+
